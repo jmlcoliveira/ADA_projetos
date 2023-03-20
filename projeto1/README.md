5177c2dd697b658dc5d5a116919979a2d2202a14
João Oliveira <jmlcoliveira@gmail.com>
2023-03-20 15:48:40 +0000

    starting point and readme

---
+++ projeto1/README.md
@@ -0,0 +1,15 @@
+Objetivo é atravessar uma serie de campos (plots)<p>
+Só podem andar com um objeto de cada vez.<p>
+Objetos estão em easy plots.<p>
+Tempos para passar:<p>
+1 -> entrar **e** sair **sem** objeto (easy plot)
+
+2-> entrar **ou** sair **com** objeto (easy plot)
+
+3-> entrar **e** sair **com** objeto (easy plot)
+
+4-> entrar com uma **harpa** e passar pelos **cães**. (plot)
+
+5-> entrar com uma **poção** e passar pelos **caes** ou pelo **troll** (plot)
+
+6-> entrar com **manto de invisibilidade** e passar por **todos** (plot)
---
+++ projeto1/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        Path p = null;
+        while(t > 0){
+            p = new Path(in.readLine());
+            System.out.println(p.getTime());
+            t--;
+        }
+    }
+}
+
+class Path {
+    private final String path;
+    Path(String path){
+        this.path = path;
+    }
+
+    public int getTime() {
+        return 0;
+    }
+}
